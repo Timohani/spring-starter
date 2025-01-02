8b6d29c64ac81181b9304d387d787eeb5fcf9a56
Timowa <sd3059272@gmail.com>
2025-01-02 21:39:27 +0500

    Fix the bug. Add logging

--- src/main/java/org/timowa/spring/config/DatabaseProperties.java
+++ src/main/java/org/timowa/spring/config/DatabaseProperties.java
@@ -5,16 +5,17 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.List;
 
 @ConfigurationProperties(prefix = "db")
-public record DatabaseProperties(String username,
-                                 String password,
-                                 String driver,
-                                 String url,
-                                 String hosts,
-                                 PoolProperties pool,
-                                 List<PoolProperties> pools
+public record DatabaseProperties(
+    String username,
+    String password,
+    String driver,
+    String url,
+    String host,
+    PoolProperties pool,
+    List<PoolProperties> pools
 ) {
-
-    private record PoolProperties(Integer size,
-                                  Integer timeout) {
-    }
+    public record PoolProperties(
+        Integer size,
+        Integer timeout
+    ) {}
 }
