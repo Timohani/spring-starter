98563853364046ac7ff5dc0afde98f80a75d1bc5
Timowa <sd3059272@gmail.com>
2025-02-15 16:03:32 +0500

    Querydsl filter

--- src/main/java/org/timowa/spring/database/repository/FilterUserRepositoryImpl.java
+++ src/main/java/org/timowa/spring/database/repository/FilterUserRepositoryImpl.java
@@ -1,13 +1,15 @@
 package org.timowa.spring.database.repository;
 
+import com.querydsl.jpa.impl.JPAQuery;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.criteria.Predicate;
 import lombok.RequiredArgsConstructor;
 import org.timowa.spring.database.entity.User;
+import org.timowa.spring.dto.QPredicates;
 import org.timowa.spring.dto.UserFilter;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import static org.timowa.spring.database.entity.QUser.user;
 
 @RequiredArgsConstructor
 public class FilterUserRepositoryImpl implements FilterUserRepository {
@@ -16,24 +18,16 @@ public class FilterUserRepositoryImpl implements FilterUserRepository {
 
     @Override
     public List<User> findAllByFilter(UserFilter filter) {
-        var cb = entityManager.getCriteriaBuilder();
-        var criteria = cb.createQuery(User.class);
+        var predicate = QPredicates.builder()
+                .add(filter.firstname(), user.firstname::containsIgnoreCase)
+                .add(filter.lastname(), user.lastname::containsIgnoreCase)
+                .add(filter.birthDate(), user.birthDate::before)
+                .build();
 
-        var user = criteria.from(User.class);
-        criteria.select(user);
-
-        List<Predicate> predicates = new ArrayList<>();
-        if (filter.firstname() != null && !filter.firstname().isBlank()) {
-            predicates.add(cb.like(user.get("firstname"), filter.firstname()));
-        }
-        if (filter.lastname() != null && !filter.lastname().isBlank()) {
-            predicates.add(cb.like(user.get("lastname"), filter.lastname()));
-        }
-        if (filter.birthDate() != null) {
-            predicates.add(cb.lessThan(user.get("birthDate"), filter.birthDate()));
-        }
-        criteria.where(predicates.toArray(Predicate[]::new));
-
-        return entityManager.createQuery(criteria).getResultList();
+        return new JPAQuery<User>(entityManager)
+                .select(user)
+                .from(user)
+                .where(predicate)
+                .fetch();
     }
 }
