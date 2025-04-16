6e4a2215acdaaafefbf53a9a40f0b8e77e52d1d7
Timowa <sd3059272@gmail.com>
2025-04-16 20:06:48 +0500

    feat: Авторизация

--- src/main/java/org/timowa/spring/config/SecurityConfiguration.java
+++ src/main/java/org/timowa/spring/config/SecurityConfiguration.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.timowa.spring.database.entity.Role;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -13,8 +15,12 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(CsrfConfigurer::disable)
+        http
+//                .csrf(CsrfConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "users/registration", "/v3/api-docs/", "/swagger-ui/").permitAll()
+                        .requestMatchers("/admin/**").hasRole(Role.ADMIN.getAuthority())
+                        .requestMatchers(antMatcher("/users/{\\d}/delete")).hasAnyAuthority(Role.ADMIN.getAuthority(), Role.OPERATOR.getAuthority())
                         .anyRequest().authenticated())
 //                .httpBasic(Customizer.withDefaults());
                 .formLogin(login -> login
--- src/main/java/org/timowa/spring/database/entity/Role.java
+++ src/main/java/org/timowa/spring/database/entity/Role.java
@@ -3,7 +3,7 @@ package org.timowa.spring.database.entity;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER, ADMIN;
+    USER, ADMIN, OPERATOR;
 
     @Override
     public String getAuthority() {
